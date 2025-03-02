d862b7bac65e7c1474a42156a65b0d04e4738882
ooijingkai <ooijingkai@gmail.com>
2025-03-02 15:53:45 +0800

    feat: init repo

--- main.go
+++ main.go
@@ -7,12 +7,10 @@ import (
 
 	// "backend/mq"
 	"backend/routes"
-	// "backend/services"
 )
 
 func main() {
 	configs.InitEnv()   // init env
-	// services.Init()     // init s3
 	// mq.Init()           // init rabbitmq connection
 	storage.GetStorageInstance()	// init db
 	routes.InitRoutes() // init controller routes
--- routes/router.go
+++ routes/router.go
@@ -54,56 +54,8 @@ func InitRoutes() {
 	itemRoute.GET("/:id", item.GetItem)
 	itemRoute.PUT("", item.UpdateItem)
 	itemRoute.DELETE("/:id", item.DeleteItem)
-
-	// // platform api
-	// platform := v1.Group("/platform")
-
-	// platformImage := platform.Group("/image")
-	// platformImage.GET("", image.GetAllImages)
-	// platformImage.GET("/:corId", image.GetImageByCorId)
-	// platformImage.GET("/name/:creatorName", image.GetImageByCreatorName)
-	// platformImage.GET("/status/:corId", process.GetProcessStatusByCorId)
-	// platformImage.POST("", image.UploadImage)
-
-	// platformChallenge := platform.Group("/challenge")
-	// platformChallenge.GET("", challenge.GetAllChallenges)
-	// platformChallenge.GET("/:corId", challenge.GetChallengeByCorID)
-	// platformChallenge.GET("/name/:creatorName", challenge.GetChallengeByCreatorName)
-	// platformChallenge.GET("/status/:corId", process.GetProcessStatusByCorId)
-	// platformChallenge.POST("", challenge.CreateChallenge)
-	// platformChallenge.GET("/attempt/:participant", attempt.GetAllAttemptsByParticipant)
-
-	// platformProcess := platform.Group("/process")
-	// platformProcess.GET("", process.GetAllProcesses)
-	// platformProcess.GET("/:corId", process.GetProcessByCorID)
-	// platformProcess.GET("/name/:creatorName", process.GetProcessByCreatorName)
-
-	// platformAttempt := platform.Group("/attempt")
-	// platformAttempt.POST("", attempt.StartAttempt)
-	// platformAttempt.GET("/status/:corId", process.GetProcessStatusByCorId)
-	// platformAttempt.GET("/:token", attempt.GetOneAttemptByToken)
-	// platformAttempt.POST("/submit", attempt.SubmitAttemptByToken)
-	// platformAttempt.GET("", attempt.GetAllAttempt)
-
-	// platformResult := platform.Group("/result")
-	// platformResult.POST("/:token")
-
-	// // // trigger api
-	// // trigger := v1.Group("/trigger")
-	// // triggerImage := trigger.Group("/image")
-	// // triggerImage.POST("", image.UploadImage)
-
-	// // // challenge
-	// // triggerChallenge := trigger.Group("/challenge")
-	// // triggerChallenge.POST("", challenge.CreateChallenge)
-
-	// // // start challenge
-	// // triggerStartChallenge := trigger.Group("/startchallenge")
-	// // triggerStartChallenge.GET("", attempt.GetAttempt)
-	// // triggerStartChallenge.POST("", attempt.StartAttempt)
-
 	// // Swagger
-	// router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	// router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler)) 
 
 	router.Run(":" + configs.PORT)
 }
