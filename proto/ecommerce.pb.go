06c27c735fd772e6ea757fcc2c0fab22161e7c0c
ooijingkai <ooijingkai@gmail.com>
2025-03-16 22:47:12 +0800

    feat: add gorm tag

--- proto/ecommerce.pb.go
+++ proto/ecommerce.pb.go
@@ -308,7 +308,7 @@ type Product struct {
 	Price           float32                `protobuf:"fixed32,3,opt,name=price,proto3" json:"price,omitempty"`
 	Inventory       int32                  `protobuf:"varint,4,opt,name=inventory,proto3" json:"inventory,omitempty"`
 	Description     string                 `protobuf:"bytes,5,opt,name=description,proto3" json:"description,omitempty"`
-	Images          []string               `protobuf:"bytes,6,rep,name=images,proto3" json:"images,omitempty"` // [bucketname+filename]
+	Images          []string               `protobuf:"bytes,6,rep,name=images,proto3" json:"images,omitempty" gorm:"type:text[]"` // [bucketname+filename]
 	StripePriceId   string                 `protobuf:"bytes,7,opt,name=stripe_price_id,json=stripePriceId,proto3" json:"stripe_price_id,omitempty"`
 	StripeProductId string                 `protobuf:"bytes,8,opt,name=stripe_product_id,json=stripeProductId,proto3" json:"stripe_product_id,omitempty"`
 	MerchantId      string                 `protobuf:"bytes,9,opt,name=merchant_id,json=merchantId,proto3" json:"merchant_id,omitempty"`
