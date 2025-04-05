fd5ab0c02b31c99d5ee7ad4df34b725c3c6f93d3
Yao Long <yaolongt@gmail.com>
2025-04-05 20:07:41 +0800

    try add logs

--- middleware/checkAuth.go
+++ middleware/checkAuth.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"backend/configs"
 	"backend/models"
+	"fmt"
 	"slices"
 
 	"github.com/gin-gonic/gin"
@@ -11,7 +12,9 @@ import (
 
 func CheckAuth(c *gin.Context) {
 	tokenString, err := c.Cookie("token")
+	fmt.Println("1", tokenString)
 	if err != nil {
+		fmt.Println("2", err)
 		c.JSON(401, gin.H{"error": "Unauthorized"})
 		c.Abort()
 		return
@@ -26,6 +29,7 @@ func CheckAuth(c *gin.Context) {
 	})
 
 	if err != nil || !token.Valid {
+		fmt.Println("3", err, !token.Valid)
 		c.JSON(401, gin.H{"error": "Unauthorized"})
 		c.Abort()
 		return
@@ -33,12 +37,14 @@ func CheckAuth(c *gin.Context) {
 
 	claims, ok := token.Claims.(jwt.MapClaims)
 	if !ok {
+		fmt.Println("4", ok, claims)
 		c.JSON(401, gin.H{"error": "Unauthorized"})
 		c.Abort()
 		return
 	}
 
 	if !claims.VerifyExpiresAt(jwt.TimeFunc().Unix(), true) {
+		fmt.Println("5", claims.VerifyExpiresAt(jwt.TimeFunc().Unix(), true))
 		c.JSON(401, gin.H{"error": "Unauthorized token expired"})
 		c.Abort()
 		return
@@ -48,6 +54,7 @@ func CheckAuth(c *gin.Context) {
 
 	authenticatedRoles := []string{string(models.RoleAdmin), string(models.RoleMerchant)}
 	if !slices.Contains(authenticatedRoles, userRole) {
+		fmt.Println("6", userRole)
 		c.JSON(401, gin.H{"error": "Unauthorized"})
 		c.Abort()
 		return
